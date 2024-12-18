372947211c3a491a5ef88dec50c01e9d72bf22a7
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-12-18 22:12:20 +0800

    系统级插件,进程查看

---
+++ function_plugins/process_info/process_info.py
@@ -0,0 +1,61 @@
+import time
+
+from common.message_send import send_message
+from common.config import config
+from scheduling.asyn_task_assignment import asyntask
+from scheduling.line_task_assignment import linetask
+
+PLUGIN_NAME = "进程显示"  # 自定义插件名称
+
+
+def del_cache(websocket, uid, nickname, gid, message_dict):
+    """
+    回显输入的内容。
+
+    :param websocket: WebSocket连接对象。
+    :param uid: 用户ID。
+    :param nickname: 用户昵称。
+    :param gid: 群组ID。
+    :param message_dict: 消息字典，包含发送的消息。
+    """
+
+    if not uid in config["admin"]:
+        return 
+
+    info = ""
+    queue_info = linetask.get_queue_info()
+    info += (f"线性队列大小: {queue_info['queue_size']}, 正在运行的任务数量: {queue_info['running_tasks_count']}\n")
+
+    for task_id, details in queue_info['task_details'].items():
+        start_time = details.get("start_time", 0)
+        end_time = details.get("end_time", 0)
+        status = details.get("status", "unknown")
+        elapsed_time = end_time - start_time if end_time else time.time() - start_time
+        info += (f"任务ID: {task_id}, 状态: {status}, 已运行时间: {elapsed_time:.2f} 秒\n")
+
+    queue_info = asyntask.get_queue_info()
+    info += (f"异步队列大小: {queue_info['queue_size']}, 正在运行的任务数量: {queue_info['running_tasks_count']}\n")
+
+    for task_id, details in queue_info['task_details'].items():
+        start_time = details.get("start_time", 0)
+        end_time = details.get("end_time", 0)
+        status = details.get("status", "unknown")
+        elapsed_time = end_time - start_time if end_time else time.time() - start_time
+        info += (f"任务ID: {task_id}, 状态: {status}, 已运行时间: {elapsed_time:.2f} 秒\n")
+
+    return send_message(websocket, uid, gid, message=info)
+
+
+def register(plugin_manager):
+    """
+    注册插件到插件管理器。
+
+    :param plugin_manager: 插件管理器实例。
+    """
+    plugin_manager.register_plugin(
+        name=PLUGIN_NAME,
+        commands=["进程信息"],
+        asynchronous=False,
+        handler=lambda websocket, uid, nickname, gid, message_dict: del_cache(websocket, uid, nickname, gid,
+                                                                              message_dict),
+    )
