b038cb3e447c937a0c8345ec08f6b6b1969c755e
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-12-19 16:27:14 +0800

    Update filter_manager.py

--- manager/filter_manager.py
+++ manager/filter_manager.py
@@ -1,17 +1,16 @@
-from common.log import logger
+import asyncio
+
 from common.config import config
+from common.log import logger
 from common.module_load import load
 from scheduling.thread_scheduling import add_task
 
 
 class FilterManager:
     def __init__(self):
-        self.filter_name = []
-        self.filters = {}
-        self.filters_asynchronous = {}
-        self.filters_func = {}
+        self.filter_info = {}
 
-    def register_filter(self, filter_name, filter_rule, asynchronous, handler):
+    def register_filter(self, filter_name, filter_rule, asynchronous, timeout_processing, handler):
         """
         注册一个新的过滤器。
         :param filter_name: 过滤器名称
@@ -22,10 +21,7 @@ class FilterManager:
             raise ValueError("Handler must be a callable function.")
         if not isinstance(filter_rule, str):
             raise ValueError("Filter rule must be a string representing a regex pattern.")
-        self.filter_name.append(filter_name)
-        self.filters[filter_name] = filter_rule
-        self.filters_asynchronous[filter_name] = asynchronous
-        self.filters_func[filter_name] = handler
+        self.filter_info[filter_name] = (filter_rule, asynchronous, timeout_processing, handler)
         logger.debug(f"FILTERS |过滤器:{filter_name}导入成功| FILTERS")
 
     def handle_message(self, websocket, uid, gid, message_dict, message, filter_name):
@@ -37,10 +33,17 @@ class FilterManager:
         :param gid: 群组ID。
         :param websocket: WebSocket连接对象。
         """
-        handler = self.filters_func[filter_name]
-        add_task(filter_name, handler, self.filters_asynchronous[filter_name], websocket, uid, gid, message,
-                 message_dict)
+        _, asynchronous, timeout_processing, handler = self.filter_info[filter_name]
+        add_task(timeout_processing, filter_name, handler, asynchronous, websocket, uid, gid, message, message_dict)
 
 
 filter_dir = config["filters_dir"]
-filter_manager = load(filter_dir, FilterManager)
+filter_manager, load_module = load(filter_dir, FilterManager)
+
+# 判断开不开启插件热加载
+enable_hot_loading = config["enable_hot_loading"]
+if enable_hot_loading:
+    from common.file_monitor import start_monitoring
+
+    # 启动插件文件夹监视
+    asyncio.run(start_monitoring(filter_dir, load_module, filter_manager))
