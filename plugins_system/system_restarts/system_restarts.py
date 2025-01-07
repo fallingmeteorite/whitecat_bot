7f7791484b9c59b87b257f52b9a06aa76cb84d15
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2025-01-07 18:12:52 +0800

    添加说明

--- plugins_system/system_restarts/system_restarts.py
+++ plugins_system/system_restarts/system_restarts.py
@@ -5,26 +5,13 @@ from common.message_send import send_message
 SYSTEM_NAME = "系统重启"  # 自定义插件名称
 
 
-def send_notification(websocket: Any, uid: str, gid: str, message: str) -> None:
-    """
-    发送通知消息到指定用户或群组。
-
-    :param websocket: WebSocket 连接对象。
-    :param uid: 用户 ID。
-    :param gid: 群组 ID。
-    :param message: 要发送的消息。
-    """
-    send_message(websocket, uid, gid, message=message)
-
-
 def write_restart_signal(gid: str) -> None:
     """
     将重启信号写入文件。
 
     :param gid: 群组 ID。
     """
-    with open('restart.txt', 'w') as f:
-        f.write(gid)  # 可以记录特定群组 ID，供后续使用
+    open('restart.txt', 'w').close()
 
 
 def system_restarts(websocket: Any, uid: str, nickname: str, gid: str, message_dict: Dict) -> None:
@@ -37,7 +24,7 @@ def system_restarts(websocket: Any, uid: str, nickname: str, gid: str, message_d
     :param gid: 群组 ID。
     :param message_dict: 消息字典，包含发送的消息。
     """
-    send_notification(websocket, uid, gid, message="所有处理线程开始重启，停止接受消息，请稍等")
+    send_message(websocket, uid, gid, message="所有处理线程开始重启，停止接受消息，请稍等")
     write_restart_signal(gid)
 
 
